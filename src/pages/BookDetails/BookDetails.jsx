8df69a0bf7c915865eb478065e91d8cb3950498d
Taricol <mozumder9122023@gmail.com>
2026-02-04 20:44:05 +0600

    params,useParams hook and fetching data dynamically with id

---
+++ src/pages/BookDetails/BookDetails.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useLoaderData, useParams } from "react-router";
+
+const BookDetails = () => {
+  const { id } = useParams();
+  const bookId = parseInt(id);
+  const data = useLoaderData();
+  const singleBook = data.find((book) => book.bookId === bookId);
+  const { bookName, image } = singleBook;
+  // console.log(singleBook);
+  //console.log(typeof id,data);
+  return (
+    <div className=" w-2/3 mx-auto">
+      <img className="w-48" src={image} alt="" />
+      <h5>{bookName}</h5>
+
+      <button className="btn btn-accent m-2">Read</button>
+      <button className="btn btn-info m-2">Wish List</button>
+    </div>
+  );
+};
+
+export default BookDetails;
